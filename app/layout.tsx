30918ba2ec98a0adab1afae4e122aac3fb2c860c
Muhammed-Hamoud <hammudmuhammed6@gmail.com>
2024-08-13 15:29:00 +0300

    Initial commit from Create Next App

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Analytics } from "@vercel/analytics/react"
+
 
 export default function RootLayout({
   children,
@@ -10,6 +12,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased `} >
         {children}
+        <Analytics />
       </body>
     </html>
   );
